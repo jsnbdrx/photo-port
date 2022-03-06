4a23763379a1e1aa2d85b9e81270d82ba5a2f97b
Jason Boudreaux <js.bdrx@gmail.com>
2022-03-05 18:12:39 -0600

    modal complete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
-import Contact from './components/Contact';
+import ContactForm from './components/Contact';
 
 function App() {
   const [categories] = useState([
@@ -35,8 +35,8 @@ function App() {
             <About></About>
           </>
         ) : (
-            <Contact></Contact>
-          )}
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
